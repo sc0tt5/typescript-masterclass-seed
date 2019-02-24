48d3a9d36e38098aca0cf019351a1d948e9e1c5a
Scott Robertson <sc0tt5@users.noreply.github.com>
2019-02-23 22:27:33 -0600

    keyof generics and lookup types

--- src/app.ts
+++ src/app.ts
@@ -8,8 +8,15 @@ const person = {
 // anonymous type
 type Person = typeof person; // type query
 type PersonKeys = keyof Person; // name | age
-
 type PersonTypes = Person[PersonKeys]; // types
+
+// generic types T and K
+// lookup type
+function getProperty<T, K extends keyof T>(obj: T, key: K) {
+	return obj[key];
+}
+
+const personName = getProperty(person, 'name');
 
 const anotherPerson: Person = {
 	name: 'John',
