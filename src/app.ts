3ef396d074f5c76e456e105f1afe2cd2684e3ba5
Scott Robertson <sc0tt5@users.noreply.github.com>
2019-02-27 20:24:05 -0600

    literal type guards and "in" operator

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,29 @@
 // type guards
-// user defined type guards
+// literal type guards and "in" operator
+
+// in operator
+// does the window object have a property called localStorage
+const exists = 'localStorage' in window;
 
 class Song {
+	kind: 'song';
 	constructor(public title: string, public duration: number) {}
 }
 
 class Playlist {
+	kind: 'playlist';
 	constructor(public name: string, public songs: Song[]) {}
 }
 
 // user defined type guard
 // if this function returns true then we are dealing with a song -- item is Song
-function isSong(item: any): item is Song {
-	return item instanceof Song;
+function isSong(item: any) {
+	return 'title' in item;
 }
 
 function getItemName(item: Song | Playlist) {
-	if (isSong(item)) {
+	// if (isSong(item)) {
+	if (item.kind === 'song') {
 		return item.title;
 	}
 	return item.name;
