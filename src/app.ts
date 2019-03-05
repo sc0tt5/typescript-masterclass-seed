4780406621c905140cbcb8cadd817c5ed339bbb1
Scott Robertson <sc0tt5@users.noreply.github.com>
2019-03-05 12:17:22 -0600

    include, exclude and file properties

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,3 @@
-// declaration files
-
-// emitting declaration files from tsc
-
-// add to tsconfig.json declaration true
-// this will auto-gnenerate your typings
-// run tsc in terminal, open dist to see app.d.ts
-// also, set types directory in like "declarationDir": "./@types",
-
-export class Foo {
-	constructor(public name: string) {}
-	bar(age: number) {}
-}
+// tsconfig and compiler options
+// include, exclude and file properties
+// files, include, and exclude properties in tsconfig.json
