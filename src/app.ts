59a3313ff33b272adcd00f0d781929be99ce6d8f
Scott Robertson <sc0tt5@users.noreply.github.com>
2019-03-09 13:35:59 -0600

    performance bundling and tslib

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,14 @@
 // tsconfig and compiler options
-// enforcing code quality
+// performance bundling and tslib
+
+// npm i tslib --save
+// by using tslib it gives us a much more performant bundle size
+
+// tsconfig
+// "noEmitHelpers": true, *NOTE: no longer needed when using importHelpers true
+// "importHelpers": true,
+
+// noEmitHelpers prevent the __extend from appearing at top of each
+// then we use tslib which includes extend and other helpers
+
+export class App {}
